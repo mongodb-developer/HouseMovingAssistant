d78e913c6bcd9367990c81361416cbfbce3d4192
Luce Carter <luce@lucyscarter.co.uk>
2022-07-15 14:25:29 -0700

    Added models for data storage

---
+++ Models/MovingTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoDB.Bson;
+using Realms;
+
+namespace HouseMovingAssistant.Models
+{
+    public class MovingTask : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        [MapTo("owner")]
+        public string Owner { get; set; }
+
+        [MapTo("name")]
+        [Required]
+        public string Name { get; set; }
+
+        [MapTo("_partition")]
+        [Required]
+        public string Partition { get; set; }
+
+        [MapTo("status")]
+        [Required]
+        public string Status { get; set; }
+
+        public enum MovingTaskStatus
+        {
+            Open,
+            InProgress,
+            Complete
+        }
+    }
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿
+using Realms;
+
+namespace HouseMovingAssistant.Models
+{
+	public class User : RealmObject
+	{
+        [PrimaryKey]
+        [MapTo("_id")]
+        [Required]
+        public string Id { get; set; }
+
+        [MapTo("_partition")]
+        [Required]
+        public string Partition { get; set; }
+
+        [MapTo("name")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
+

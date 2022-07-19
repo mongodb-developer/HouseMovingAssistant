2581348498a85235e8b8a5bb898cd6c20b80c4dc
Luce Carter <luce@lucyscarter.co.uk>
2022-07-19 18:28:39 +0100

    Added personalised welcome message when logged in

--- ViewModels/MovingTasksPageViewModel.cs
+++ ViewModels/MovingTasksPageViewModel.cs
@@ -2,21 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using MvvmHelpers;
 
 namespace HouseMovingAssistant.ViewModels
 {
-    [INotifyPropertyChanged]
-    public partial class MovingTasksPageViewModel
+   
+    public partial class MovingTasksPageViewModel : BaseViewModel
     {
         public MovingTasksPageViewModel()
         {
-            
+            WelcomeMessage = $"Welcome in {App.RealmApp.CurrentUser.Profile.Email}!";
         }
 
-       
-      
+        private string welcomeMessage = "Welcome in!";
+        public string WelcomeMessage
+        {
+            get => welcomeMessage;
+            set => SetProperty(ref welcomeMessage, value);
+        }
+
+        private string movingTaskEntryText = "";
+        public string MovingTaskEntryText
+        {
+            get => movingTaskEntryText;
+            set => SetProperty(ref movingTaskEntryText, value);
+        }
+
     }
 }
